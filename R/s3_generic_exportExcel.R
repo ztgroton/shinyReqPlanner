1ec3bd3a534ec6d684931cbd6cfc87b9e1eed607
ztgroton <ztgroton@gmail.com>
2024-05-15 10:45:00 -0400

    feat: s3 classes 'docFuncReq', 'docNonFuncReq', 'docTestCases'

--- R/s3_generic_exportExcel.R
+++ R/s3_generic_exportExcel.R
@@ -26,3 +26,55 @@ exportExcel.docTbl <- function(obj, file, ...) {
   openxlsx::write.xlsx(x = file_data, file = file, ..., colWidths = 'auto')
 
 }
+
+#' S3 Method - Export S3 Class 'docUserStory' to Excel
+#'
+#' @inheritParams exportExcel
+#' @export
+exportExcel.docUserStory <- function(obj, file, ...) {
+
+  if (missing(obj)) {stop("`obj` is missing", call. = TRUE)}
+  if (missing(file)) {stop("`file` is missing", call. = TRUE)}
+
+  NextMethod()
+
+}
+
+#' S3 Method - Export S3 Class 'docFuncReq' to Excel
+#'
+#' @inheritParams exportExcel
+#' @export
+exportExcel.docFuncReq <- function(obj, file, ...) {
+
+  if (missing(obj)) {stop("`obj` is missing", call. = TRUE)}
+  if (missing(file)) {stop("`file` is missing", call. = TRUE)}
+
+  NextMethod()
+
+}
+
+#' S3 Method - Export S3 Class 'docNonFuncReq' to Excel
+#'
+#' @inheritParams exportExcel
+#' @export
+exportExcel.docNonFuncReq <- function(obj, file, ...) {
+
+  if (missing(obj)) {stop("`obj` is missing", call. = TRUE)}
+  if (missing(file)) {stop("`file` is missing", call. = TRUE)}
+
+  NextMethod()
+
+}
+
+#' S3 Method - Export S3 Class 'docTestCases' to Excel
+#'
+#' @inheritParams exportExcel
+#' @export
+exportExcel.docTestCases <- function(obj, file, ...) {
+
+  if (missing(obj)) {stop("`obj` is missing", call. = TRUE)}
+  if (missing(file)) {stop("`file` is missing", call. = TRUE)}
+
+  NextMethod()
+
+}
